9057eda50ebd4e4eaaafc4fc6bbb64f2dc531c2b
Tarun Gupta <tg.whiteforce@gmail.com>
2023-11-25 16:40:53 -0800

    signup page

--- src/app/services/forms.service.ts
+++ src/app/services/forms.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { Injectable } from "@angular/core";
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FormsService {
-
   loginForm!: UntypedFormGroup;
+  signpForm!: UntypedFormGroup;
 
-  constructor(private formBuilder:UntypedFormBuilder) {}
+  constructor(private formBuilder: UntypedFormBuilder) {}
 
   initializeForm() {
     this.loginForm = this.formBuilder.group({
-      email: ['',[Validators.email, Validators.required]],
-      password:['',[Validators.required]]
-    })
+      email: ["", [Validators.email, Validators.required]],
+      password: ["", [Validators.required]],
+    });
+
+    this.signpForm = this.formBuilder.group({
+      first_name: ["", [Validators.required]],
+      last_name: ["", [Validators.required]],
+      email: ["", [Validators.required]],
+      fullName: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+    });
   }
 }
